29933a954d56ef1f4ef0343ee57b86967365b8f4
Marek Olšák <m_olsak@utb.cz>
2021-11-30 11:04:23 +0100

    Functional release.

--- CSharp_Steganography_WPF/Views/ExtractMessagePage.xaml.cs
+++ CSharp_Steganography_WPF/Views/ExtractMessagePage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using CSharp_Steganography_WPF.ViewModels;
 using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace CSharp_Steganography_WPF.Views
 {
@@ -10,11 +11,36 @@ namespace CSharp_Steganography_WPF.Views
     {
         readonly MainWindowViewModel mainWindowViewModel;
 
+        private double _zoomValue = 1.0;
+
         public ExtractMessagePage(MainWindowViewModel mainWindowViewModel)
         {
             this.mainWindowViewModel = mainWindowViewModel;
             DataContext = mainWindowViewModel;
             InitializeComponent();
+        }
+
+        private void Image_MouseWheel(object sender, System.Windows.Input.MouseWheelEventArgs e)
+        {
+            if (e.Delta > 0)
+            {
+                _zoomValue += 0.05;
+            }
+            else
+            {
+                _zoomValue -= 0.05;
+                if (_zoomValue < 0.05)
+                    _zoomValue = 0.05;
+            }
+            Image? image = sender as Image;
+            if (image is null)
+            {
+                e.Handled = true;
+                return;
+            }
+            ScaleTransform scale = new ScaleTransform(_zoomValue, _zoomValue);
+            image.LayoutTransform = scale;
+            e.Handled = true;
         }
     }
 }
